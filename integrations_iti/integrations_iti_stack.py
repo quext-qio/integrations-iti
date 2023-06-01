d463c5cfb4ee7cb76f56542191b7c5e0a7269a0c
Randald Villegas <randald1991@gmail.com>
2023-06-01 11:26:11 -0600

    feature (Guest Cards): Initial structure
    Co-Authored-By: Kevin Brenes <89096003+kevin-brenes@users.noreply.github.com>
    Co-Authored-By: Ingrid Marenco Hernández <97114640+IngridMH07@users.noreply.github.com>

--- integrations_iti/integrations_iti_stack.py
+++ integrations_iti/integrations_iti_stack.py
@@ -4,16 +4,25 @@ from aws_cdk import (
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from stacks_definition.guestcards import GuestCard
 
 class IntegrationsItiStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "IntegrationsItiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # -----------------------------------------------------------------------
+        # Guestcard 
+        # TODO: load env from Parameter Store
+        environment={
+            "LOG_LEVEL": "INFO",
+            "JIRA_REPORTER_TOKEN": "ATATT3xFfGF0jPYyx6LBni9zxGQByh3O7ojXxj2NS3YGosM5H0lEyWlvYl6XI-_j4luNaqfNORreXa1PpQg6gSp_LXuZ9jq8DQKAl4ZpX_QMFCwHrJhRvZuLejVGpSIYClhXeqBZ5M1f7RC851JBtYdcuwVrbtyJZLklNUfihHwudACRnbN8EIw=6416F982",
+        }
+        GuestCard.create_definition(
+            stage_name="dev", 
+            api_version="v1", 
+            allow_methods=['OPTIONS', 'GET', 'POST'], 
+            environment=environment
+        )
+        
+        
