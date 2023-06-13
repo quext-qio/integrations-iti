784cba718a984b68ab9916fb4e8c6e761dd0926c
Randald Villegas <randald1991@gmail.com>
2023-06-13 12:23:27 -0600

    feature (API Gateway): :hammer: New structure for share an api gateway in all lambdas :octocat:

--- src/stacks/units_stack/units_stack.py
+++ src/stacks/units_stack/units_stack.py
@@ -8,28 +8,26 @@ from constructs import Construct
 
 class UnitsStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, api: apigateway_.RestApi , **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # -----------------------------------------------------------------------
-        # Guestcard 
-        # TODO: load environment from Parameter Store
-        # Constants
+        # Units 
         environment={
             "LOG_LEVEL": "INFO",
             "TEST": "test",
         }
-        stage_name="dev"
         timeout=Duration.seconds(900)
         allow_methods=['OPTIONS', 'POST']
         
         # --------------------------------------------------------------------
         # Create Cerberus Layers
+
         cerberus_layer = lambda_.LayerVersion(
             self, "CerberusLayer",
             layer_version_name="CerberusLayer",
             description="Package documentation: https://docs.python-cerberus.org/en/stable/",
-            code=lambda_.Code.from_asset("./utils/layers/cerberus_layer.zip"),
+            code=lambda_.Code.from_asset("src/utils/layers/cerberus_layer.zip"),
             compatible_runtimes=[
                 lambda_.Runtime.PYTHON_3_10, 
                 lambda_.Runtime.PYTHON_3_9, 
@@ -52,31 +50,15 @@ class UnitsStack(Stack):
             environment=environment,
             runtime=lambda_.Runtime.PYTHON_3_10,
             timeout=timeout,
-            code=lambda_.Code.from_asset("./lambdas/units"),
+            code=lambda_.Code.from_asset("./src/lambdas/units"),
             handler="lambda_function.lambda_handler",
             layers=[cerberus_layer],
             function_name="Units_Lambda_Function",
         )
 
-        # --------------------------------------------------------------------
-        # Create a Rest API instance
-        base_api = apigateway_.RestApi(
-            self, "Integrations_Api", 
-            rest_api_name="Integrations_Api", 
-            description="Base API Gateway for Zato to AWS Migration",
-            deploy_options=apigateway_.StageOptions(
-                stage_name=stage_name, 
-                logging_level=apigateway_.MethodLoggingLevel.INFO,
-                data_trace_enabled=True,
-            ),
-            endpoint_configuration=apigateway_.EndpointConfiguration(
-                types=[apigateway_.EndpointType.REGIONAL]
-            ),
-        )
-
         # -------------------------------------------------------------------- 
         # Add a resource to the base API and configure CORS options for the resource 
-        general_units_endpoint = base_api.root.add_resource("api").add_resource("v2").add_resource("general").add_resource(
+        general_units_endpoint = api.add_resource("general").add_resource(
             "units",
             default_cors_preflight_options=apigateway_.CorsOptions(
                 allow_methods=allow_methods,
@@ -88,7 +70,7 @@ class UnitsStack(Stack):
         # Create a Lambda integration instance
         endpoint_lambda_integration = apigateway_.LambdaIntegration(
             lambda_function,
-            proxy=False,
+            proxy=True,
             integration_responses=[
                 apigateway_.IntegrationResponse(
                     status_code="200",
@@ -102,7 +84,6 @@ class UnitsStack(Stack):
 
         # --------------------------------------------------------------------
         # Add a POST method to endpoint
-        # TODO: Add a method response for the all endpoint's status code
         general_units_endpoint.add_method(
             'POST', 
             endpoint_lambda_integration,
--- tests/unit/test_integrations_iti_stack.py
+++ tests/unit/test_integrations_iti_stack.py
@@ -1,13 +1,12 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
-from integrations_iti.integrations_iti_stack import IntegrationsItiStack
+from src.stacks.guestcards_stack.guestcards_stack import GuestcardsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in integrations_iti/integrations_iti_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = IntegrationsItiStack(app, "integrations-iti")
+    stack = GuestcardsStack(app, "GuestcardsStackTest")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
