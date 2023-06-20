2947ca7234915d866f9eaf75c9049661e7c9ca57
Randald Villegas <randald1991@gmail.com>
2023-06-20 16:48:47 -0600

    reafact (Placepay token): Get token ready :alien:

--- src/lambdas/placepay/get_lambda_function.py
+++ src/lambdas/placepay/get_lambda_function.py
@@ -1,59 +1,27 @@
-import place
 import json
-from Schemas.SchemaRequestPost import SchemaRequestGet
-from Utils.Response import Response
+import place
+from Schemas.SchemaRequestGet import SchemaRequestGet
 from Config.Config import config
 
 def lambda_handler(event, context):
-        query_params = event['queryStringParameters']
-        is_valid, input_errors = SchemaRequestGet(query_params).is_valid()
-        if is_valid:
-            try:
-                place.api_key = config['ApiKey']
-                access_token = place.AccessToken.create(
-                    account_id = query_params["accountId"],
-                    type='session_access'
-                )
+    query_params = event['queryStringParameters']
+    is_valid, input_errors = SchemaRequestGet(query_params).is_valid()
+    if is_valid:
+        try:
+            place.api_key = config['ApiKey']
+            access_token = place.AccessToken.create(
+                account_id = query_params["accountId"],
+                type='session_access'
+            )
 
-                # Case: Success
-                return {
-                    'statusCode': "200",
-                    'body': json.dumps({
-                        'data': [
-                            access_token.access_token
-                        ],
-                        'errors': []
-                    }),
-                    'headers': {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',  
-                    },
-                    'isBase64Encoded': False  
-                }
-            
-            except Exception as e:
-                # Case: Unhandled error
-                return {
-                    'statusCode': "500",
-                    'body': json.dumps({
-                        'data': [],
-                        'errors': f"{e}",
-                    }),
-                    'headers': {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*',  
-                    },
-                    'isBase64Encoded': False  
-                }
-            
-        else:
-            # Case: Bad Request
-            errors = [{"message": f"{k}, {v[0]}" } for k,v in input_errors.items()]
+            # Case: Success
             return {
-                'statusCode': "400",
+                'statusCode': "200",
                 'body': json.dumps({
-                    'data': [],
-                    'errors': errors,
+                    'data': [
+                        access_token.access_token
+                    ],
+                    'errors': []
                 }),
                 'headers': {
                     'Content-Type': 'application/json',
@@ -61,3 +29,34 @@ def lambda_handler(event, context):
                 },
                 'isBase64Encoded': False  
             }
+        
+        except Exception as e:
+            # Case: Unhandled error
+            return {
+                'statusCode': "500",
+                'body': json.dumps({
+                    'data': [],
+                    'errors': f"{e}",
+                }),
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',  
+                },
+                'isBase64Encoded': False  
+            }
+        
+    else:
+        # Case: Bad Request
+        errors = [{"message": f"{k}, {v[0]}" } for k,v in input_errors.items()]
+        return {
+            'statusCode': "400",
+            'body': json.dumps({
+                'data': [],
+                'errors': errors,
+            }),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',  
+            },
+            'isBase64Encoded': False  
+        }
