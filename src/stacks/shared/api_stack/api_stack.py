199554284ab88f3c799874108b36066cf1960499
Randald Villegas <randald1991@gmail.com>
2023-06-21 11:17:30 -0600

    refact (parameter store): Shared environments for all Stacks

--- src/stacks/shared/api_stack/api_stack.py
+++ src/stacks/shared/api_stack/api_stack.py
@@ -3,13 +3,14 @@ from aws_cdk import (
     aws_apigateway as apigateway_,
 )
 from constructs import Construct
+from src.utils.enums.stage_name import StageName
 
 class APIStack(Stack):
     @property
     def get_api(self):
         return self.api
 
-    def __init__(self, scope: Construct, construct_id: str, stage_name: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, stage_name: StageName, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # --------------------------------------------------------------------
@@ -19,7 +20,7 @@ class APIStack(Stack):
             rest_api_name="Integrations_Api", 
             description="Base API Gateway for Zato to AWS Migration",
             deploy_options=apigateway_.StageOptions(
-                stage_name=stage_name, 
+                stage_name=stage_name.value, 
                 logging_level=apigateway_.MethodLoggingLevel.INFO,
                 data_trace_enabled=True,
             ),
