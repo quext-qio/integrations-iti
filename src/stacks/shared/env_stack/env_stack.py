5f39bba0df9852633be6dd4121cc4366411339b7
Randald Villegas <randald1991@gmail.com>
2023-06-28 10:18:47 -0600

    refact (Env Stack): Assuming role for get environment values from parameter store

--- src/stacks/shared/env_stack/env_stack.py
+++ src/stacks/shared/env_stack/env_stack.py
@@ -25,8 +25,30 @@ class EnvStack(Stack):
         else:
             raise Exception("Invalid stage name")
 
-        session = boto3.Session(profile_name='shared_acc')
-        ssm_client = session.client(
+        # Load session using default AWS credentials
+        session = boto3.Session(profile_name='default')
+        
+        # Create a client to interact with the STS (Security Token Service)
+        sts_client = session.client("sts")
+        
+        # Assume the IAM role
+        response = sts_client.assume_role(
+            RoleArn="arn:aws:iam::273056594042:role/cdk-integrationApi-get-ssm-parameters",
+            RoleSessionName=f"{stage_name.value}-assumed-session"
+        )
+
+        # Extract the temporary credentials from the response
+        credentials = response['Credentials']
+
+        # Create a new session using the temporary credentials
+        new_session = boto3.Session(
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+        )
+        
+        # Get the environmet values from Parameter Store using the assumed session
+        ssm_client = new_session.client(
             "ssm", 
             region_name="us-east-1", 
         )
