cfae829d9309771fbf45008b17ccb79a678b4766
Kevin Brenes <89096003+kevin-brenes@users.noreply.github.com>
2023-07-11 21:11:36 -0600

    transunion: validation updates

--- src/lambdas/transunion/schemas/IdentitySchema.py
+++ src/lambdas/transunion/schemas/IdentitySchema.py
@@ -1,25 +1,33 @@
 from cerberus import Validator
+from datetime import datetime
+
+from Utils.IdentityErrorHandler import IdentityErrorHandler
+from Utils.CustomErrorHandler import CustomErrorHandler
 
 
 class ValidationConstants:
+    ALPHA = "^[A-Za-z\'\- ]+$"
+    ALPHANUMERIC = "^[A-Za-z0-9]+$"
+    INTEGERS = "^[0-9]+$"
+    LETTERS = "^[a-zA-Z]+$"
+    DATE = "^(0[1-9]|1[0-2])(0[1-9]|1\d|2\d|3[01])(19|20)\d{2}$"
     PHONE = '^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$'
     EMAIL = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
+    ADDRESS = "^[A-Za-z0-9\s]+$"
 
 
 class IdentitySchema:
     def __init__(self, _data):
-        print(_data)
         self.schema_request_post_verify = {
             "CustomParameters": {
                 'required': False,
-                'empty': True,
-                'nullable': True,
-                'type': 'list'
+                'type': 'list',
+                'nullable': True
             },
             "DeviceSessionId": {
                 'required': False,
-                'nullable': True,
-                'type': 'string'
+                'type': 'string',
+                'empty': True
             },
             "Applicant": {
                 'required': True,
@@ -27,50 +35,82 @@ class IdentitySchema:
                 'schema': {
                     "FirstName": {
                         'required': True,
-                        'type': 'string'
+                        'type': 'string',
+                        'regex': ValidationConstants.ALPHA,
+                        'minlength': 1,
+                        'maxlength': 15
                     },
                     "LastName": {
                         'required': True,
-                        'type': 'string'
+                        'type': 'string',
+                        'regex': ValidationConstants.ALPHA,
+                        'minlength': 1,
+                        'maxlength': 25
                     },
                     "MiddleName": {
+                        'required': False,
+                        'type': 'string',
                         'empty': True,
-                        'type': 'string'
+                        'regex': ValidationConstants.ALPHA,
+                        'maxlength': 15
                     },
                     "Suffix": {
+                        'required': False,
+                        'type': 'string',
                         'empty': True,
-                        'type': 'string'
+                        'regex': ValidationConstants.ALPHANUMERIC,
+                        'maxlength': 3
                     },
                     "BirthDate": {
                         'required': True,
-                        'type': 'string'
+                        'type': 'string',
+                        'regex': ValidationConstants.DATE,
+                        'minlength': 8,
+                        'maxlength': 8
                     },
                     "Ssn": {
                         'required': True,
-                        'type': 'string'
+                        'type': 'string',
+                        'regex': ValidationConstants.INTEGERS,
+                        'minlength': 9,
+                        'maxlength': 9
                     },
                     "Phone": {
                         'required': True,
                         'type': 'string',
-                        'regex': ValidationConstants.PHONE
+                        'regex': ValidationConstants.PHONE,
+                        'minlength': 10,
+                        'maxlength': 10
                     },
                     "EmailAddress": {
                         'required': True,
-                        'regex': ValidationConstants.EMAIL
+                        'regex': ValidationConstants.EMAIL,
+                        'maxlength': 100
                     },
                     "IdentificationNumber": {
-                        'required': True,
-                        'type': 'string'
-                    },
-                    "IdentificationType": {
+                        'required': False,
                         'type': 'string',
                         'empty': True,
+                        'regex': ValidationConstants.ALPHANUMERIC,
+                        'maxlength': 50
+                    },
+                    "IdentificationType": {
+                        'required': False,
+                        'type': 'string',
+                        'empty': True,
+                        'maxlength': 50,
+                        'allowed': ['DriversLicense', 'GovernmentIssuedPicture', 'MilitaryId', 'Visa']
                     },
                     "IdentificationState": {
-                        'required': True,
-                        'type': 'string'
+                        'required': False,
+                        'type': 'string',
+                        'empty': True,
+                        'minlength': 2,
+                        'maxlength': 2,
+                        'regex': ValidationConstants.LETTERS
                     },
                     "CustomParameters": {
+                        'required': False,
                         'type': 'list',
                         'nullable': True,
                     },
@@ -82,27 +122,41 @@ class IdentitySchema:
                             'schema': {
                                 "StreetAddress": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'regex': ValidationConstants.ADDRESS,
+                                    'minlength': 1,
+                                    'maxlength': 100
                                 },
                                 "City": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'regex': ValidationConstants.ALPHA,
+                                    'minlength': 1,
+                                    'maxlength': 27
                                 },
                                 "State": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'regex': ValidationConstants.LETTERS,
+                                    'minlength': 2,
+                                    'maxlength': 2
                                 },
                                 "PostalCode": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'regex': ValidationConstants.INTEGERS,
+                                    'minlength': 5,
+                                    'maxlength': 5
                                 },
                                 "Country": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'allowed': ['USA']
                                 },
                                 "AddressType": {
                                     'required': True,
-                                    'type': 'string'
+                                    'type': 'string',
+                                    'allowed': ['Current', 'Previous']
                                 }
                             }
                         }
@@ -128,22 +182,22 @@ class IdentitySchema:
                             'type': 'string'
                         }
                     }
-
                 }
             },
             "ReferenceNumber": {
                 'required': True,
-                'type': 'string'
+                'type': 'string',
+                'regex': ValidationConstants.ALPHANUMERIC,
+                'meta': {'regex': "ReferenceNumber can only contain letters and numbers."},
+                'minlength': 8,
+                'maxlength': 24
             },
             "CustomParameters": {
                 'required': False,
-                'type': 'list',
-                'empty': True,
-                'nullable': True
+                'type': 'list'
             },
             "DeviceSessionId": {
                 'required': False,
-                'nullable': True,
                 'type': 'string'
             }
         }
@@ -151,13 +205,35 @@ class IdentitySchema:
 
     def is_valid_verify(self):
         validator = Validator(self.schema_request_post_verify)
+
         if validator.validate(self.data):
+            current_date_time = datetime.now()
+            value_date_time = datetime.strptime(self.data['Applicant']['BirthDate'], "%m%d%Y") if "BirthDate" in self.data["Applicant"] else ""
+            
+            # extra validation to check the BirthDate since it should be less than the current date
+            if value_date_time != "" and value_date_time > current_date_time:
+                return False, {
+                    "data": {},
+                    "error": {
+                        "Applicant": [
+                            {
+                                "BirthDate": [
+                                    "BirthDate should be less than the current date."
+                                ]
+                            }
+                        ]
+                    }
+                }
+
             return True, {}
         else:
-            return False, validator.errors
+            error_handler = IdentityErrorHandler(validator.errors)
+
+            return False, error_handler.update_error_messages()
 
     def is_valid_evaluate(self):
-        validator = Validator(self.schema_request_post_evaluate)
+        validator = Validator(self.schema_request_post_evaluate, error_handler=CustomErrorHandler(self.schema_request_post_evaluate))
+
         if validator.validate(self.data):
             return True, {}
         else:
--- src/lambdas/transunion/schemas/ScreeningSchema.py
+++ src/lambdas/transunion/schemas/ScreeningSchema.py
@@ -1,6 +1,13 @@
 from cerberus import Validator
 
 
+class ValidationConstants:
+    ALPHA = "^[A-Za-z.'\- ]*$"
+    STREET_ADDRESS = "^[A-Za-z0-9 .,'()\x2D]*$"
+    CITY = "^[A-Za-z .,'()\s-]*$"
+    ALLOWED_STATES = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY", "AA", "AE", "AP"]
+
+
 class ScreeningSchema:
     def __init__(self, _data):
         print(_data)
@@ -12,17 +19,23 @@ class ScreeningSchema:
                 'schema': {
                     "rentAmount": {
                         'required': True,
-                        'type': 'integer'
+                        'type': 'integer',
+                        'min': 0,
+                        'max': 25000
                     },
                     "depositAmount": {
                         'required': True,
                         'type': 'integer',
-                        'default': 1
+                        'default': 1,
+                        'min': 0,
+                        'max': 50000
                     },
                     "leaseTerm": {
                         'required': True,
                         'type': 'integer',
-                        'default': 12
+                        'default': 12,
+                        'min': 1,
+                        'max': 999
                     },
                     "photoIdVerified": {
                         'required': True,
@@ -41,15 +54,20 @@ class ScreeningSchema:
                                     'schema': {
                                         'applicantType': {
                                             'required': True,
-                                            'type': 'integer'
+                                            'type': 'integer',
+                                            'allowed': [1, 2]
                                         },
                                         'firstName': {
                                             'required': True,
-                                            'type': 'string'
+                                            'type': 'string',
+                                            'minlength': 2,
+                                            'regex': ValidationConstants.ALPHA
                                         },
                                         'lastName': {
                                             'required': True,
-                                            'type': 'string'
+                                            'type': 'string',
+                                            'minlength': 2,
+                                            'regex': ValidationConstants.ALPHA
                                         },
                                         'dateOfBirth': {
                                             'required': True,
@@ -57,34 +75,40 @@ class ScreeningSchema:
                                         },
                                         'streetAddress': {
                                             'required': True,
-                                            'type': 'string'
+                                            'type': 'string',
+                                            'regex': ValidationConstants.STREET_ADDRESS
                                         },
                                         'city': {
                                             'required': True,
-                                            'type': 'string'
+                                            'type': 'string',
+                                            'minlength': 3,
+                                            'regex': ValidationConstants.CITY
                                         },
                                         'state': {
                                             'required': True,
-                                            'type': 'string'
+                                            'type': 'string',
+                                            'allowed': ValidationConstants.ALLOWED_STATES
                                         },
                                         'postalCode': {
                                             'required': True,
-                                            'type': 'integer'
+                                            'type': 'integer',
+                                            'check_with': self.check_postal_code_length
                                         },
                                         'employmentStatus': {
                                             'required': True,
-                                            'type': 'integer'
+                                            'type': 'integer',
+                                            'allowed': [1, 2, 3, 4]
                                         },
                                         'employmentIncome': {
                                             'required': True,
-                                            'min': 1,
                                             'type': 'integer',
                                             'default': 1
                                         },
                                         'employmentIncomePeriod': {
                                             'required': True,
                                             'type': 'integer',
-                                            'default': 1
+                                            'default': 1,
+                                            'allowed': [1, 2, 3]
                                         }
                                     }
                                 }
@@ -96,9 +120,77 @@ class ScreeningSchema:
         }
         self.data = _data
 
+    def check_postal_code_length(self, field, value, error):
+        if len(str(value)) != 5 and len(str(value)) != 9:
+            error(field, f"{field} length is limited to 5 or 9 characters.")
+
     def is_valid(self):
         validator = Validator(self.schema_request_post)
         if validator.validate(self.data):
+            applicant_type = False
+            applicant_errors = []
+
+            # extra validations required
+            for index, applicant in enumerate(self.data["application"]["applicants"]["applicant"]):
+                # checks if theres at least one applicant with applicantType = 1.
+                if applicant["applicantType"] == 1 and applicant_type == False:
+                    applicant_type = True
+                
+                # employmentIncome validations
+                if applicant["employmentStatus"] == 1 and applicant["employmentIncome"] <= 0:
+                    applicant_errors.append({
+                        str(index): [
+                            {
+                                "employmentIncome": [
+                                    "Required to be > 0 when employmentStatus = 1."
+                                ]
+                            }
+                        ]
+                    })
+                elif applicant["employmentStatus"] != 1 and applicant["employmentIncome"] < 0:
+                    applicant_errors.append({
+                        str(index): [
+                            {
+                                "employmentIncome": [
+                                    "Required to be >= 0."
+                                ]
+                            }
+                        ]
+                    })
+
+            # applicantType error handler
+            if applicant_type == False:
+                return False, {
+                    "application": [
+                        {
+                            "applicants": [
+                                {
+                                    "applicant": [
+                                        {
+                                            "applicantType": [
+                                                "A valid request must include at least one applicant with an applicantType = 1."
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            # employmentIncome error handler
+            elif len(applicant_errors) > 0:
+                return False, {
+                    "application": [
+                        {
+                            "applicants": [
+                                {
+                                    "applicant": applicant_errors
+                                }
+                            ]
+                        }
+                    ]
+                }
+
             return True, {}
         else:
             return False, validator.errors
