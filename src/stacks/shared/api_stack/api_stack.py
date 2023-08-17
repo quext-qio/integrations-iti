97c186ae0f16d79674a257fcf260f20c78551be7
Randald Villegas <randald1991@gmail.com>
2023-08-16 18:09:38 -0600

    feature (custom domain): Testing custom domain for dev

--- src/stacks/shared/api_stack/api_stack.py
+++ src/stacks/shared/api_stack/api_stack.py
@@ -43,41 +43,6 @@ class APIStack(NestedStack):
     def __init__(self, scope: Construct, construct_id: str, stage: StageName, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         # --------------------------------------------------------------------
-        # --------------------------------------------------------------------
-        # Test custom domain
-        domain_name = "d-kept97rbkf.execute-api.us-east-1.amazonaws.com"
-        hosted_zone_id = "Z1UJRXOUMOOFQ8"
-        certificate_arn = "arn:aws:acm:us-east-1:633546161654:certificate/eb1794a2-4724-475a-9aad-b9e5bdaa38e3"
-        
-        role_arn = os.getenv('ROLE_ARN', 'arn:aws:iam::273056594042:role/cdk-integrationApi-get-ssm-parameters')
-        assumed_session = self.assume_role(role_arn, stage)
-        acm_client = assumed_session.client('acm', region_name='us-east-1')
-        certificate = acm_.Certificate.from_certificate_arn(self, "MyCertificate", certificate_arn)
-        
-        # custom_domain = self.api.add_domain_name("CustomDomain",
-        #     domain_name=domain_name,
-        #     certificate=certificate,
-        #     endpoint_type=apigateway_.EndpointType.REGIONAL,
-        #     security_policy=apigateway_.SecurityPolicy.TLS_1_2
-        # )
-
-        # hosted_zone = route53_.HostedZone.from_hosted_zone_attributes(self, "HostedZone",
-        #     hosted_zone_id=hosted_zone_id,
-        #     zone_name="integrations-api.dev.quext.io"
-        # )
-
-        # route53_.ARecord(self, "ApiGatewayAliasRecord",
-        #     target=route53_.RecordTarget.from_alias(route53targets_.ApiGatewayDomain(custom_domain)),
-        #     zone=hosted_zone
-        # )
-
-        # CfnOutput(self, "ApiUrl",
-        #     value=custom_domain.domain_name + "/{proxy+}",
-        #     description="URL of the API with custom domain"
-        # )
-
-
-        # --------------------------------------------------------------------
         # Create a Rest API instance
         self.api = apigateway_.RestApi(
             self, "Integrations_Api", 
@@ -95,11 +60,26 @@ class APIStack(NestedStack):
                 types=[apigateway_.EndpointType.REGIONAL],
             ),
         )
+
         # --------------------------------------------------------------------
-        custom_domain_name = apigateway_.DomainName.from_domain_name_attributes(self, "DomainName",
-            domain_name=domain_name,
+        # Test custom domain
+        # TODO: Make for all stages
+
+        # domain_name = "d-kept97rbkf.execute-api.us-east-1.amazonaws.com"
+        # certificate_arn = "arn:aws:acm:us-east-1:633546161654:certificate/eb1794a2-4724-475a-9aad-b9e5bdaa38e3"
+        # custom_url = "integrations-api.dev.quext.io"
+        # certificate = acm_.Certificate.from_certificate_arn(self, "MyCertificate", certificate_arn)
+
+        hosted_zone_id = "Z1UJRXOUMOOFQ8"
+        domain_name_alias_target = "integrations-api.dev.quext.io"
+        custom_domain_name = "dev.quext.io"
+        
+        
+        custom_domain_name = apigateway_.DomainName.from_domain_name_attributes(
+            self, "DomainName",
+            domain_name=custom_domain_name,
             domain_name_alias_hosted_zone_id=hosted_zone_id,
-            domain_name_alias_target=domain_name
+            domain_name_alias_target=domain_name_alias_target,
         )
 
         apigateway_.BasePathMapping(self, "BasePathMapping",
@@ -162,38 +142,6 @@ class APIStack(NestedStack):
             "v2": dict_v2,
         }
 
-        # # --------------------------------------------------------------------
-        # # Assume the IAM role
-        # role_arn = os.getenv('ROLE_ARN', 'arn:aws:iam::273056594042:role/cdk-integrationApi-get-ssm-parameters')
-        # assumed_session = self.assume_role(role_arn, stage)
-
-
-        # # Use the assumed_session to create the ACM certificate instance
-        # acm_certificate_arn = "arn:aws:acm:us-east-1:633546161654:certificate/eb1794a2-4724-475a-9aad-b9e5bdaa38e3"
-        # custom_domain_name = "d-kept97rbkf.execute-api.us-east-1.amazonaws.com"
-        # acm_client = assumed_session.client('acm', region_name='us-east-1')
-        # certificate = acm_client.get_certificate(CertificateArn=acm_certificate_arn)
-
-
-        # # Create a DomainName resource
-        # domain_name = apigateway_.DomainName(
-        #     self, f"{stage.value}-CustomDomainName",
-        #     domain_name=custom_domain_name,
-        #     certificate=certificate,
-        #     endpoint_type=apigateway_.EndpointType.REGIONAL,
-        # )
-
-        # # Create a BasePathMapping to associate the DomainName with your API stages
-        # for custom_stage_name, stage_resources in self.resources.items():
-        #     for resource_key, resource in stage_resources.items():
-        #         base_path_mapping = apigateway_.BasePathMapping(
-        #             self, f"{custom_stage_name}-{resource_key}-Mapping",
-        #             domain_name=domain_name,
-        #             rest_api=self.api,
-        #             stage=resource.node.try_get_context('stage_name'),
-        #             base_path=resource_key
-        #         )
-        
         # --------------------------------------------------------------------
         # TODO: Remove logic when custom domain is ready
         # Create a Lambda function for Store API URL after deployment
@@ -201,37 +149,3 @@ class APIStack(NestedStack):
         print_url_lambda = print_api_url_lambda(self, api_url, stage)
         rule = events_.Rule(self, f"{stage.name}-PrintApiUrlRule", schedule=events_.Schedule.expression('rate(365 days)'))
         rule.add_target(targets_.LambdaFunction(print_url_lambda, event=events_.RuleTargetInput.from_object({})))
-
-
-    # Method to assume a role and return a new session
-    def assume_role(self, role_arn: str, stage_name: StageName):
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-        aws_session_token = os.getenv('AWS_SESSION_TOKEN')
-
-        session = boto3.Session(
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            aws_session_token=aws_session_token,
-        )
-
-        # Create a client to interact with the STS (Security Token Service)
-        sts_client = session.client("sts")
-
-        # Assume the IAM role
-        response = sts_client.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName=f"apigateway-{stage_name.value}-assumed-session"
-        )
-
-        # Extract the temporary credentials from the response
-        credentials = response['Credentials']
-
-        # Create a new session using the temporary credentials
-        new_session = boto3.Session(
-            aws_access_key_id=credentials['AccessKeyId'],
-            aws_secret_access_key=credentials['SecretAccessKey'],
-            aws_session_token=credentials['SessionToken'],
-        )
-
-        return new_session
