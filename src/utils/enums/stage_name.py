8f30034238d2d630781354017f4d0f17fab32a1e
Randald Villegas <randald1991@gmail.com>
2023-08-18 12:25:30 -0600

    refact (Stages Domain Name): Add new environments config

--- src/utils/enums/stage_name.py
+++ src/utils/enums/stage_name.py
@@ -22,25 +22,22 @@ class StageName(Enum):
                 "custom_domain_name" : "integrations-api-qa.dev.quext.io",
             }
         elif self == StageName.STAGE:
-            raise Exception("Custom Domain Name not implemented for 'stage' yet") 
+            return {
+                "hosted_zone_id" : "Z1UJRXOUMOOFQ8",
+                "domain_name_alias_target" : "stage.quext.io",
+                "custom_domain_name" : "integrations-api.stage.quext.io",
+            }
         elif self == StageName.RC:
-            raise Exception("Custom Domain Name not implemented for 'rc' yet")
+            return {
+                "hosted_zone_id" : "Z1UJRXOUMOOFQ8",
+                "domain_name_alias_target" : "stage.quext.io",
+                "custom_domain_name" : "integrations-api-rc.stage.quext.io",
+            }
         elif self == StageName.PROD:
-            raise Exception("Custom Domain Name not implemented for 'prod' yet")
-        else:
-            raise ValueError(f"Unknown stage: {self.value}")
-        
-    # Returns the API Key depending of the Stage
-    def get_api_key(self):
-        if self == StageName.DEV:
-            return "dev-api-key"
-        elif self == StageName.QA:
-            return "qa-api-key"
-        elif self == StageName.STAGE:
-            return "stage-api-key"
-        elif self == StageName.RC:
-            return "rc-api-key"
-        elif self == StageName.PROD:
-            return "prod-api-key"
+            return {
+                "hosted_zone_id" : "Z1UJRXOUMOOFQ8",
+                "domain_name_alias_target" : "quext.io",
+                "custom_domain_name" : "integrations-api.quext.io",
+            }
         else:
             raise ValueError(f"Unknown stage: {self.value}")
