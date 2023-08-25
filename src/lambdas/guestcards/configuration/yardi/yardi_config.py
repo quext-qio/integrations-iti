7cb91f32e80b377b0047f01e3a1412062dab30d2
Randald Villegas <randald1991@gmail.com>
2023-08-25 16:38:35 -0600

    removed environments not longer needed

--- src/lambdas/guestcards/configuration/yardi/yardi_config.py
+++ src/lambdas/guestcards/configuration/yardi/yardi_config.py
@@ -8,13 +8,7 @@ config = {
     'server_name': os.environ['YARDI_SERVER_NAME'],
     'database': os.environ['YARDI_DATABASE'],
     'interface_license': os.environ['YARDI_INTERFACE_LICENSE'],
-    'user_name_demo': os.environ['YARDI_USER_NAME_DEMO'],
-    'password_demo': os.environ['YARDI_PASSWORD_DEMO'],
-    'server_name_demo': os.environ['YARDI_SERVER_NAME_DEMO'],
-    'database_demo': os.environ['YARDI_DATABASE_DEMO'],
-    'interface_license_demo': os.environ['YARDI_INTERFACE_LICENSE_DEMO'],
     'leasing_url': os.environ['LEASING_HOST'],
     'ips_host': os.environ['IPS_HOST'],
     'yardi_url': os.environ['YARDI_URL'],
-    'yardi_url_demo': os.environ['YARDI_URL_DEMO']
 }
