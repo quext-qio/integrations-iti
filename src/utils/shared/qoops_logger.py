ba90e6e427136a1d137d5f642410c0ce72b4d4b5
Randald Villegas <randald1991@gmail.com>
2023-10-05 10:06:21 -0600

    refact (env): Getting custom domain name by os to call qoops in same env

--- src/utils/shared/qoops_logger.py
+++ src/utils/shared/qoops_logger.py
@@ -95,8 +95,8 @@ class Logger():
         logger.setLevel(logging.INFO)
 
         # Create file handler that logs error and higher level messages 
-        file_handler = logging.FileHandler('automation.log')
-        file_handler.setLevel(logging.ERROR)
+        # file_handler = logging.FileHandler('automation.log')
+        # file_handler.setLevel(logging.ERROR)
 
         # Create console handler for all logs
         console_handler = logging.StreamHandler()
@@ -107,10 +107,10 @@ class Logger():
 
         # Set formatter in file and console
         console_handler.setFormatter(formatter)
-        file_handler.setFormatter(formatter)
+        #file_handler.setFormatter(formatter)
 
         # Add the handlers to logger
         logger.addHandler(console_handler)
-        logger.addHandler(file_handler)
+        #logger.addHandler(file_handler)
         
         return logger
