18d703f5eacacad16f28d996f1c761317b2e3e4a
Randald Villegas <randald1991@gmail.com>
2023-10-05 09:33:12 -0600

    feature (logger): Add logger to connect with Qoops

--- src/lambdas/v1/trupay_google/job_lambda_function.py
+++ src/lambdas/v1/trupay_google/job_lambda_function.py
@@ -1,18 +1,59 @@
 import json, requests
 from datetime import datetime
 
+# ----------------------------------------------------------------------------------------
 def lambda_handler(event, context):
     now = datetime.now()
     print(f"TruPay Google Lambda Function: {now.strftime('%d/%m/%Y %H:%M:%S')}")
 
     # Get all users information from TruPay API
+    trupay_users = get_trupay_users()
 
     # Get all users information from Google API
+    google_users = get_google_users()
 
     # Loop through all users on TruPay API and check if they exist on Google API
+    for trupay_user in trupay_users:
+        # Search for user on Google API
+        #google_user = next((user for user in google_users if user['email'] == trupay_user['email']), None)
+    
+        # If user does not exist on Google API, create user on Google API, 
+        # else update user on Google API if there are any changes
+        # Print information on users that were created or updated on Google API to CloudWatch
+        pass
 
-    # If user does not exist on Google API, create user on Google API, 
-    # else update user on Google API if there are any changes
 
-    # Print information on users that were created or updated on Google API to CloudWatch
+# ----------------------------------------------------------------------------------------
+# Get all users information from TruPay API
+def get_trupay_users():
+    # Get users from TruPay API
 
+    # Map users to Google API format in Object ()
+    return []
+
+# ----------------------------------------------------------------------------------------
+# Get all users information from Google API
+def get_google_users():
+    # Get users from Google API
+
+    # Map users to Google API format in Object ()
+    return []
+
+# ----------------------------------------------------------------------------------------
+# Create user on Google API
+def create_google_user(trupay_user):
+    try:
+        # Create user on Google API
+        return True, {}
+    except Exception as e:
+        return False, {"error": str(e)}
+
+# ----------------------------------------------------------------------------------------
+# Update user on Google API
+def update_google_user(google_user, trupay_user):
+    try:
+        # Update user on Google API
+        return True, {}
+    except Exception as e:
+        return False, {"error": str(e)}
+# ----------------------------------------------------------------------------------------
---
+++ src/utils/shared/qoops_logger.py
@@ -0,0 +1,115 @@
+import logging
+import inspect
+import json
+import requests
+import os
+
+# ----------------------------------------------------------------------------
+class QoopsLogger(logging.Logger):
+    """
+    Class to get information about errors ans critical issues
+    """
+    url = "https://integrations-api.dev.quext.io/api/v2/jira"
+    headers = {'Content-Type': 'application/json'}
+    stage = os.environ['STAGE']
+
+    def __init__(self, name=inspect.stack()[1][3], level=logging.DEBUG):
+        return super(QoopsLogger, self).__init__(name, level)
+
+    # Method to handle errors
+    def error(self, msg, *args, **kwargs):
+        # Error case:
+        print(f"\nGenerating ticket: {msg}: {self.name} - Environment {self.stage}")
+        payload = self.create_payload(
+            msg,
+            issue_type="Bug",
+            priority=3,
+            project_name="QIN",
+        )
+        information_detail = self.call_jira_endpoint(msg, payload)
+        return super(QoopsLogger, self).error(information_detail, *args, **kwargs)
+
+    # Method to handle critical issues
+    def critical(self, msg, *args, **kwargs):
+        # Critical case:
+        print(f"\nGenerating ticket: {msg}: {self.name} - Environment {self.stage}")
+        payload = self.create_payload(
+            msg,
+            issue_type="Bug",
+            priority=5,
+            project_name="QIN",
+        )
+        information_detail = self.call_jira_endpoint(msg, payload)
+        return super(QoopsLogger, self).critical(information_detail, *args, **kwargs)
+
+    # Create payload for jira required input
+    def create_payload(
+        self,
+        msg,
+        issue_type="Bug",
+        priority=3,
+        project_name="QIN",
+    ):
+        payload = json.dumps({
+            "project_name": project_name,
+            "ticket_summary": f"Issue in Service {self.name} - Environment {self.stage}",
+            "ticket_description": f"Ticket is automatically generated for issues found in {self.name}",
+            "issue_type": issue_type,
+            "priority": priority,
+            "labels": ["Qoops", f"{self.name}-{self.stage}", "Jira-Automation", "ITI-Service"],
+            "list_issues": [msg],
+            "testing": "true"
+        })
+        return payload
+
+    # Method to report issues
+    def call_jira_endpoint(self, msg, payload):
+        information_detail = f"{msg}."
+        response = requests.request(
+            "POST",
+            self.url,
+            headers=self.headers,
+            data=payload,
+        )
+
+        # If get success response from Jira SQS, will add ticket's id into log detail
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            information_detail = information_detail + f" SQS Response {data}"
+        return information_detail
+
+
+# ----------------------------------------------------------------------------
+class Logger():
+    """
+    Class to allow logic for creation of .log files
+    """
+    
+    def instance(self, service_name, level=logging.INFO):
+        # Set custom class to handle error and critical issues
+        logging.setLoggerClass(QoopsLogger)
+
+        # Get logger by service name  
+        logger = logging.getLogger(f"{service_name}")
+        logger.setLevel(logging.INFO)
+
+        # Create file handler that logs error and higher level messages 
+        file_handler = logging.FileHandler('automation.log')
+        file_handler.setLevel(logging.ERROR)
+
+        # Create console handler for all logs
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+
+        # Create formatter
+        formatter = logging.Formatter('AUTOMATION_LOG %(asctime)s - %(levelname)s - %(name)s : %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+        # Set formatter in file and console
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+        # Add the handlers to logger
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+        
+        return logger
