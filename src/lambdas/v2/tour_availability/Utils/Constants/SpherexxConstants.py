00f3f1ddf0ae23918d68199eb2e67dbe1e9031a7
IngridMH07 <ingrid.marenco@oktara.com>
2023-10-17 15:04:21 -0600

    Added spherexx available-times endpoints

---
+++ src/lambdas/v2/tour_availability/Utils/Constants/SpherexxConstants.py
@@ -0,0 +1,4 @@
+SLOTS_PATH = "GetOpenAppointmentSlots"
+INSERT_LEAD_PATH = "InsertLead"
+HOST = "www.iloveleasing.com"
+URL = "https://www.iloveleasing.com/console/int/mits4/"
---
+++ src/lambdas/v2/tour_availability/configuration/spherexx_config.py
@@ -0,0 +1,8 @@
+import os
+
+spherexx_config = {
+    "spherexx_username": os.environ['SPHEREXX_USERNAME'],
+    "spherexx_password": os.environ['SPHEREXX_PASSWORD']
+}
+
+
