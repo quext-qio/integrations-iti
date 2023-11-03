1e758d6c3d1d7498602827319f3557a55f54d629
Randald Villegas <randald1991@gmail.com>
2023-11-03 13:06:11 -0600

    refact (remove old code): Remove code not longer need

--- src/lambdas/residents/Utils/Config/Config.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-config = {
-    "Newco_host": os.environ['NEWCO_DB_HOST'],
-    "Newco_password": os.environ['NEWCO_DB_PASSWORD'],
-    "Newco_db_name": os.environ['NEWCO_DB_NAME'],
-    "Newco_db_user": os.environ['NEWCO_DB_USER'],
-} 
--- src/stacks/shared/vpc_stack/vpc_stack.py
+++ src/stacks/shared/vpc_stack/vpc_stack.py
@@ -2,9 +2,6 @@ from constructs import Construct
 from aws_cdk import (
     NestedStack,
     aws_ec2 as ec2_,
-    aws_lambda as lambda_,
-    aws_apigateway as apigateway_,
-    Duration,
 )
 from src.utils.enums.app_environment import AppEnvironment
 
@@ -13,32 +10,22 @@ class VpcStack(NestedStack):
     @property
     def vpc(self):
         return self._vpc
-    
+
     @property
     def vpc_subnets(self):
         return self._vpc_subnets
-    
+
     @property
     def security_groups(self):
         return self._security_groups
 
     def __init__(
-        self, scope: Construct, 
-        construct_id: str, 
-        layers: list, 
-        api: apigateway_.RestApi,
-        environment: dict[str, str], 
-        app_environment: AppEnvironment, 
+        self, scope: Construct,
+        construct_id: str,
+        app_environment: AppEnvironment,
         **kwargs,
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # -----------------------------------------------------------------------
-        # Constants
-        timeout = Duration.seconds(900)
-        allow_methods = ['OPTIONS', 'GET']
-
-
         # --------------------------------------------------------------------
         # Read VPC by id
         vpc_id = app_environment.get_vpc_id()
@@ -53,7 +40,8 @@ class VpcStack(NestedStack):
 
         # --------------------------------------------------------------------
         # Subnet selection
-        private_subnets = vpc.select_subnets(subnet_type=ec2_.SubnetType.PRIVATE_WITH_EGRESS)
+        private_subnets = vpc.select_subnets(
+            subnet_type=ec2_.SubnetType.PRIVATE_WITH_EGRESS)
         subnet_ids = []
         if len(private_subnets.subnets) == 0:
             raise Exception(
@@ -74,71 +62,10 @@ class VpcStack(NestedStack):
         self._vpc_subnets = vpc_subnets
 
         # --------------------------------------------------------------------
-        # TODO: Read Security Group by id
+        # Read Security Group by id
         security_group_id = app_environment.get_security_group_id()
         security_group = ec2_.SecurityGroup.from_security_group_id(
             self, f"{app_environment.get_stage_name()}-iti-security-group",
             security_group_id=security_group_id,
         )
         self._security_groups = [security_group]
-
-        # --------------------------------------------------------------------
-        # Create lambda function instance test VPC
-        lambda_function = lambda_.Function(
-            self,
-            f"{app_environment.get_stage_name()}-vpc-lambda",
-            description="This Lambda is responsible to test VPC",
-            environment=environment,
-            runtime=lambda_.Runtime.PYTHON_3_10,
-            timeout=timeout,
-            code=lambda_.Code.from_asset("./src/lambdas/v1/vpc"),
-            handler="lambda_function.lambda_handler",
-            layers=layers,
-            function_name=f"{app_environment.get_stage_name()}-vpc-lambda",
-            vpc=vpc,
-            vpc_subnets=vpc_subnets,
-            security_groups=[security_group],
-        )
-
-        # --------------------------------------------------------------------
-        # Resource to test VPC (GET)
-        endpoint = api.add_resource(
-            "vpc",
-            default_cors_preflight_options=apigateway_.CorsOptions(
-                allow_methods=allow_methods,
-                allow_origins=apigateway_.Cors.ALL_ORIGINS
-            ),
-        )
-
-        # --------------------------------------------------------------------
-        # Create a Lambda integration instance
-        # GET
-        endpoint_lambda_integration = apigateway_.LambdaIntegration(
-            lambda_function,
-            proxy=True,
-            integration_responses=[
-                apigateway_.IntegrationResponse(
-                    status_code="200",
-                    response_templates={"application/json": ""},
-                    response_parameters={
-                        'method.response.header.Access-Control-Allow-Origin': "'*'"
-                    }
-                ),
-            ],
-        )
-
-        # --------------------------------------------------------------------
-        # Add a GET method to endpoint
-        endpoint.add_method(
-            'GET',
-            endpoint_lambda_integration,
-            request_parameters={},
-            method_responses=[
-                apigateway_.MethodResponse(
-                    status_code="200",
-                    response_parameters={
-                        'method.response.header.Access-Control-Allow-Origin': True
-                    }
-                )
-            ],
-        )
