1a793b07d37b601c23f2a4ddfea600347b2b2258
Randald Villegas <randald1991@gmail.com>
2023-11-10 14:27:05 -0600

    refact (conservice): Added logger

--- src/lambdas/v2/conservice/services/charges_code_service.py
+++ src/lambdas/v2/conservice/services/charges_code_service.py
@@ -1,9 +1,11 @@
-import json, requests
+import json
+import requests
 from abstract.service_interface import ServiceInterface
 from constants.constants import Constants
 
+
 class ChargesCodeService(ServiceInterface):
-    def get_data(self, body: dict):
+    def get_data(self, body: dict, logger):
         parameter = body[Constants.PARAMETER]
         conservice_outgoing = f'{Constants.HOST}{Constants.PATH}/{parameter}'
         headers = {
@@ -12,10 +14,18 @@ class ChargesCodeService(ServiceInterface):
 
         try:
             # Call conservice outgoing
-            response = requests.get(conservice_outgoing, headers=headers, params= body)
+            logger.info(
+                f'Calling Conservice endpoint {conservice_outgoing} with payload: {body} to get charges code')
+            response = requests.get(
+                conservice_outgoing,
+                headers=headers,
+                params=body
+            )
+            logger.info(
+                f'Conservice response status code: {response.status_code}')
             response_data = json.loads(response.text)
+            logger.info(f'Conservice response data: {response_data}')
 
-       
             return {
                 'statusCode': Constants.HTTP_GOOD_RESPONSE_CODE,
                 'body': json.dumps({
@@ -24,12 +34,14 @@ class ChargesCodeService(ServiceInterface):
                 }),
                 'headers': {
                     'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',  
+                    'Access-Control-Allow-Origin': '*',
                 },
-                'isBase64Encoded': False  
+                'isBase64Encoded': False
             }
 
         except Exception as e:
+            logger.error(
+                f'Error trying to call Conservice endpoint {conservice_outgoing}: {e}')
             return {
                 'statusCode': Constants.HTTP_BAD_RESPONSE_CODE,
                 'body': json.dumps({
@@ -42,8 +54,7 @@ class ChargesCodeService(ServiceInterface):
                 }),
                 'headers': {
                     'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',  
+                    'Access-Control-Allow-Origin': '*',
                 },
-                'isBase64Encoded': False  
+                'isBase64Encoded': False
             }
-    
