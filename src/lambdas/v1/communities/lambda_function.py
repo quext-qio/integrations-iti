a38c9ffd63df755829be053e09a5b0f48fcaa6a7
Randald Villegas <randald1991@gmail.com>
2023-11-10 12:35:38 -0600

    refact (engrain): Add logger to report issues

--- src/lambdas/v1/communities/lambda_function.py
+++ src/lambdas/v1/communities/lambda_function.py
@@ -1,8 +1,14 @@
 import json
 from DataPushPullShared.DataController import DataController
-#from cerberus import Validator
+from qoops_logger import Logger
+
+# ----------------------------------------------------------------------------------------
+# Create Logger instance
+logger = Logger().instance(f"(ITI) General Communities Lambda")
+
 
 def lambda_handler(event, context):
+    logger.info(f"Executing with event: {event}, context: {context}")
     headers = event['headers']
     wsgi_input = {
         'PATH_INFO': event['resource'],
@@ -10,15 +16,16 @@ def lambda_handler(event, context):
     }
     if 'x-api-key' in headers:
         wsgi_input['HTTP_X_API_KEY'] = headers['x-api-key']
-        
+
     input = json.loads(event['body'])
-    code, data_controller = DataController("info").get_communities(input.get("customerUUID",""), wsgi_input)
+    code, data_controller = DataController("info").get_communities(
+        input.get("customerUUID", ""), wsgi_input)
     return {
         'statusCode': code,
         'body':  json.dumps(data_controller),
         'headers': {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',  
+            'Access-Control-Allow-Origin': '*',
         },
-        'isBase64Encoded': False  
+        'isBase64Encoded': False
     }
