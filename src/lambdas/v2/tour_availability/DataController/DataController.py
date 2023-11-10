9f0e33815badd1c8402a06b973426b807f44695b
IngridMH07 <ingrid.marenco@oktara.com>
2023-11-10 13:10:42 -0600

    Added filter for available-dates

--- src/lambdas/v2/tour_availability/DataController/DataController.py
+++ src/lambdas/v2/tour_availability/DataController/DataController.py
@@ -1,13 +1,27 @@
+from datetime import datetime
+import pytz
+
 class DataController:
     def __init__(self, errors):
         self.errors = errors
 
     def built_response(self, data):
-        # build proper response structure for every partner
         code = 502 if self.errors else 200
+        # build proper response structure for every partner
+        
+        # Set the time zone to Central Standard Time (CST)
+        cst_timezone = pytz.timezone('America/Chicago')
+
+        # Get the current time in CST
+        current_time = datetime.now(cst_timezone).strftime("%Y-%m-%d %H:%M:%S")
+        
+        # Filter dates greater than or equal to the current time
+        available_dates = [date for date in original_response["data"]["availableTimes"] if date >= current_time]
+
+
         response = { 
             "data": {   
-                "availableTimes": data   
+                "availableTimes": available_dates   
             },  
             "error": self.errors    
         }
