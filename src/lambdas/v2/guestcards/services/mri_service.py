576f4172507062b7b9866a858c2d6745bc89a8d3
IngridMH07 <ingrid.marenco@oktara.com>
2023-11-24 09:25:41 -0600

    added tour code

--- src/lambdas/v2/guestcards/services/mri_service.py
+++ src/lambdas/v2/guestcards/services/mri_service.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from abstract.service_interface import ServiceInterface
 from utils.mapper.bedroom_mapping import bedroom_mapping
 from utils.service_response import ServiceResponse
+from services.shared.quext_tour_service import QuextTourService
 from constants.mri_constants import *
 
 class MRIService(ServiceInterface):
@@ -12,13 +13,43 @@ class MRIService(ServiceInterface):
     def get_data(self, body, ips, logger):
         logger.info(f"Getting data from MRI")
 
-        transformed_body = self.transform_payload(body)
-        
-        
-        pass
+        transformed_body = self.transform_payload(body, ips)
+        available_times = []
+        tour_scheduled_id = ""
+        tour_error = ""
+        appointment_date = ""
 
 
-    def transform_payload(payload, ips):
+        if "tourScheduleData" in body:
+                appointment_date = body["tourScheduleData"]["start"]
+                if appointment_date != "":
+                    converted_date = datetime.strptime(appointment_date.replace(
+                        "T", " ")[0:appointment_date.index("Z")].strip(), '%Y-%m-%d %H:%M:%S')
+                    format_date = converted_date.strftime("%B %d, %Y")
+                    hour = converted_date.strftime("%I:%M:%S %p")
+                    code, quext_response = QuextTourService.save_quext_tour(
+                        body)
+                    if code != 200:
+                        tour_error = quext_response["error"]["message"]
+                        headers = {
+                            'Access-Control-Allow-Origin': '*',
+                            'Content-Type': 'application/json',
+                        }
+                        available_times = QuextTourService.get_available_times(
+                            body["platformData"], body["tourScheduleData"]["start"], "Quext", headers)
+                    else:
+                        tour_scheduled_id = quext_response["data"]["id"]
+                        tour_comment = f' --TOURS--Tour Scheduled for {format_date} at {hour}'
+
+        host = "http://{machineName}/api/applications/Integrations/RM/Leasing/GuestCard"
+
+        
+
+        
+        
+
+
+    def transform_payload(self, payload, ips):
         move_in_date = payload["guestPreference"]["moveInDate"]
         visit_date = ""
         if "tourScheduleData" in payload:
