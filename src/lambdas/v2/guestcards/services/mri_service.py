49cbfe8410c0ddff53f1c1ad6b54ee54ab9b217e
IngridMH07 <ingrid.marenco@oktara.com>
2023-11-24 08:41:22 -0600

    Added transform_date function

--- src/lambdas/v2/guestcards/services/mri_service.py
+++ src/lambdas/v2/guestcards/services/mri_service.py
@@ -11,4 +11,34 @@ class MRIService(ServiceInterface):
 
     def get_data(self, body, ips, logger):
         logger.info(f"Getting data from MRI")
+
+        transformed_body = self.transform_payload(body)
+        
+        
         pass
+
+
+    def transform_payload(payload, ips):
+        move_in_date = payload["guestPreference"]["moveInDate"]
+        visit_date = ""
+        if "tourScheduleData" in payload:
+            visit_date = payload["tourScheduleData"]["start"][: payload["tourScheduleData"]["start"].index("T")]
+
+
+        transformed_data = {
+            "propertyId": ips["platformData"]["foreign_community_id"],
+            "firstName": payload["guest"]["first_name"],
+            "lastName": payload["guest"]["last_name"],
+            "comment": payload["guestComment"],
+            "emailAddress": payload["guest"]["email"],
+            "type": "P",
+            "cellPhoneNumber": payload["guest"]["phone"],
+            "beds": len(payload["guestPreference"]["desiredBeds"]),
+            "baths": payload["guestPreference"]["desiredBaths"][0], 
+            "estimatedMoveInDate": move_in_date[ :move_in_date.index("T")],  # Extracting the date part
+            "totalOccupants": payload["guestPreference"]["noOfOccupants"],
+            "desiredLeaseTerm": payload["guestPreference"]["leaseTermMonths"],
+            "visitDate": visit_date 
+        }
+
+        return transformed_data
